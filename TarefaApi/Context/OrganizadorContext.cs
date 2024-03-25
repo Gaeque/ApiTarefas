edd953195a82ba1eb39da654c9c1433b77d7791e
Gaeque <Gaeque.CF@hotmail.com>
2024-03-25 14:22:54 -0300

    Add project files.

---
+++ TarefaApi/Context/OrganizadorContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TarefaApi.Models;
+
+namespace TarefaApi.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { 
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ TarefaApi/Models/EnumStatusTarefa.cs
@@ -0,0 +1,8 @@
+﻿namespace TarefaApi.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado 
+    }
+}
